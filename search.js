4e031524daef26837a20ba07bd6bcd068ef0392c
Sanket <sanketbhat882002@gmail.com>
2023-07-25 22:07:57 +0530

    clean Up

--- search.js
+++ search.js
@@ -1,4 +1,5 @@
 const { chromium } = require('playwright');
+const fs = require('fs');
 
 async function performSearch(prompt, numPages = 20) {
   const browser = await chromium.launch({ headless: true });
@@ -17,11 +18,28 @@ async function performSearch(prompt, numPages = 20) {
   }
 
   await browser.close();
+
+  // Read existing data from test.json
+  let jsonData = {};
+  if (fs.existsSync('test.json')) {
+    const rawData = fs.readFileSync('test.json');
+    jsonData = JSON.parse(rawData);
+  }
+
+  // Append the new prompt and search results
+  const promptKey = `prompt-${Object.keys(jsonData).length + 1}`;
+  const resultsKey = `results-${Object.keys(jsonData).length + 1}`;
+  jsonData[promptKey] = prompt;
+  jsonData[resultsKey] = searchResults;
+
+  // Write the updated data back to test.json
+  fs.writeFileSync('test.json', JSON.stringify(jsonData, null, 2));
+
   return searchResults;
-  
 }
 
 module.exports = { performSearch };
+
 
 
 // const { webkit } = require('playwright'); // Use 'webkit' instead of 'chromium' for regular browser
